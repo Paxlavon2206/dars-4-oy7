8b94f00658811eedd082163cab80fcd7ae4623b9
Paxlavon2206 <paxlavon2206@gmail.com>
2024-03-13 08:01:10 +0500

    dars-4-oy7

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { StoreCard } from "../components/store-card";
-
 export const Cart = () => {
   const { products } = useSelector((state) => state.cart);
   return (
-    <div className="container bg-slate-100">
+    <div className="container bg-slate-100 pt-4 pb-4">
       {products.map((item) => (
         <StoreCard key={item.id} {...item} />
       ))}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "../components/card";
 import { Link } from "react-router-dom";
+import { FaCartPlus } from "react-icons/fa6";
 
 export const Home = () => {
   const [data, setData] = React.useState([]);
@@ -14,7 +15,7 @@ export const Home = () => {
   }, []);
   return (
     <div className="container pt-5 mb-5">
-      <Link className=" py-2 px-3 rounded-[26px] hover:bg-red-700 bg-red-500 text-white " to="/cart">Korzinka</Link>
+     <button className=" bg-violet-500 text-white py-2 px-5 rounded-[25px] "> <Link  to="/cart"><FaCartPlus/></Link></button>
       <div className="grid grid-cols-4 mt-3">
         {data.map((item) => (
           <Card key={item.id} {...item} />
